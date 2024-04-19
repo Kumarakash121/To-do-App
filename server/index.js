0c02be1cc1d81e73c54e0972180ec5ebef67b9aa
Akash <kumarakash121005@gmail.com>
2024-04-19 19:09:44 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import connectDB from "./config/db.js";
 import cors from "cors";
 connectDB()
 app.use(cors({
-    origin:["https://to-do-app-frontend-liart.vercel.app"],
+    origin:'*',
     methods:["POST","GET"],
     credentials:true,
 }));
