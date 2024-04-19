e1a9232f982629dfdba0bb9674eb07bbb597f7d3
Akash <kumarakash121005@gmail.com>
2024-04-19 19:28:54 +0530

    cors 3

--- server/app.js
+++ server/app.js
@@ -5,24 +5,31 @@ import express from "express";
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 const app = express()
-// app.use(cors(({
-//     origin:["https://to-do-app-frontend-liart.vercel.app"],
-//     methods:["POST","GET","PUT","DELETE"],
-//     credentials:true,
-// })));
-const corsOpts = {
-    origin: '*',
+// // app.use(cors(({
+// //     origin:["https://to-do-app-frontend-liart.vercel.app"],
+// //     methods:["POST","GET","PUT","DELETE"],
+// //     credentials:true,
+// // })));
+// const corsOpts = {
+//     origin: '*',
   
-    methods: [
-      'GET',
-      'POST',
-    ],
+//     methods: [
+//       'GET',
+//       'POST',
+//     ],
   
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-app.use(cors(corsOpts));
+//     allowedHeaders: [
+//       'Content-Type',
+//     ],
+//   };
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+// app.use(cors(corsOpts));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
--- server/index.js
+++ server/index.js
@@ -2,19 +2,26 @@ import app from './app.js'
 import connectDB from "./config/db.js";
 import cors from "cors";
 connectDB()
-const corsOpts = {
-    origin: '*',
+// const corsOpts = {
+//     origin: 'https://to-do-app-frontend-liart.vercel.app',
   
-    methods: [
-      'GET',
-      'POST',
-    ],
+//     methods: [
+//       'GET',
+//       'POST',
+//     ],
   
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-app.use(cors(corsOpts));
+//     allowedHeaders: [
+//       'Content-Type',
+//     ],
+//   };
+// app.use(cors(corsOpts));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.listen(4000, () => {
     console.log('server is up')
