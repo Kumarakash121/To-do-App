5675bfebe040e266d4f07cdf88044dd6b4c99618
Akash <kumarakash121005@gmail.com>
2024-04-19 19:14:37 +0530

    Cors Policy

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ import app from './app.js'
 import connectDB from "./config/db.js";
 import cors from "cors";
 connectDB()
-app.use(cors({
-    origin:'*',
-    methods:["POST","GET"],
-    credentials:true,
-}));
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+app.use(cors(corsOpts));
 app.listen(4000, () => {
     console.log('server is up')
 })
