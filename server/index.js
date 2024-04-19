f31df4b892736a6f4a28c35f4cd6ecdda639a231
Akash <kumarakash121005@gmail.com>
2024-04-19 21:22:28 +0530

    cors 4

--- server/index.js
+++ server/index.js
@@ -6,7 +6,18 @@ app.use(cors({
     origin:"https://to-do-app-frontend-liart.vercel.app",
     credentials:true,
 }))
-
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['https://to-do-app-frontend-liart.vercel.app',''];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 
 app.listen(4000, () => {
     console.log('server is up')
